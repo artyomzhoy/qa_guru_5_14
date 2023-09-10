4044b29f60d7143ec465733ca73d18c5ddfe4dae
Artyom Samoshin <artyomzhoy@gmail.com>
2023-09-10 15:49:05 +0300

    eddit conftest

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from utils import attach
 
 
-@pytest.fixture(scope='session',
+@pytest.fixture(scope='function',
                 autouse=True,
                 params=[(1920, 1080), (1600, 900), (1366, 768), (375, 667), (667, 375), (480, 800)],
                 ids=['desktop', 'desktop', 'desktop', 'mobile', 'mobile', 'mobile'])
